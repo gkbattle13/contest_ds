e1192780d9388220b53603aa47a4aaad41960d93
aaric <qlhan@incarcloud.com>
2020-02-28 18:35:45 +0800

    0.1.0-SNAPSHOT 调整代码结构

---
+++ ds_web_back/src/main/java/com/incar/contest_ds/ContestDsApplication.java
@@ -0,0 +1,13 @@
+package com.incar.contest_ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContestDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContestDsApplication.class, args);
+	}
+
+}
