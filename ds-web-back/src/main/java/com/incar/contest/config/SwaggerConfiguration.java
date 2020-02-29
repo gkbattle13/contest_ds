8deb4ee8edc4aff49fbc99bba881d4c33f14f017
kdl1217 <kamil9660@163.com>
2020-02-29 19:52:19 +0800

    add swagger config

--- ds-web-back/src/main/java/com/incar/contest/config/SwaggerConfiguration.java
+++ ds-web-back/src/main/java/com/incar/contest/config/SwaggerConfiguration.java
@@ -1,22 +1,16 @@
-package com.incarcloud.config;
+package com.incar.contest.config;
 
-import com.incarcloud.common.share.Constant;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.async.DeferredResult;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Swagger配置类
@@ -32,14 +26,13 @@ public class SwaggerConfiguration {
     public Docket createRestApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
-                .globalOperationParameters(operationParameters())
 
                 .genericModelSubstitutes(DeferredResult.class)
                 .useDefaultResponseMessages(false)
                 .forCodeGeneration(true)
                 .pathMapping("/")// base，最终调用接口后会和paths拼接在一起
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.incarcloud"))
+                .apis(RequestHandlerSelectors.basePackage("com.incar"))
                 .build();
     }
 
@@ -50,46 +43,10 @@ public class SwaggerConfiguration {
      */
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("智联钥匙API文档-智联钥匙")
-                .description("本文档包含智联钥匙与TSP平台API数据接口。")
+                .title("英卡竞赛数据流项目API文档")
+                .description("本文档包含英卡竞赛数据流项目API数据接口。")
                 .version("1.0.0-SNAPSHOT") // 版本
                 .build();
-    }
-
-    /**
-     * 全局配置参数说明
-     *
-     * @return
-     */
-    private List<Parameter> operationParameters() {
-        // Header 请求参数
-        List<Parameter> params = new ArrayList<>();
-        params.add(getParameterBuilder(Constant.HeaderParam.SMART_KEY_IMEI, "国际移动设备识别码", "header").build());
-        params.add(getParameterBuilder(Constant.HeaderParam.SMART_KEY_TS, "时间戳", "header").build());
-        params.add(getParameterBuilder(Constant.HeaderParam.SMART_KEY_SIGN, "签名", "header").build());
-        params.add(getParameterBuilder(Constant.HeaderParam.SMART_KEY_TYPE, "操作类型", "header").build());
-        params.add(getParameterBuilder(Constant.HeaderParam.SMART_KEY_CODE, "校验码", "header").build());
-
-        // 版本控制
-        ParameterBuilder versionParameterBuilder = new ParameterBuilder();
-        versionParameterBuilder.name("apiVersion").description("API版本信息")
-                .modelRef(new ModelRef("string"))
-                .parameterType("query")
-                .required(false);
-        // 组装
-        params.add(versionParameterBuilder.build());
-
-        // 国际化
-        ParameterBuilder i18nParameterBuilder = new ParameterBuilder();
-        i18nParameterBuilder.name(LocaleChangeInterceptor.DEFAULT_PARAM_NAME).description("国际化参数，默认zh_CN(简体中文), en_US(英文)")
-                .modelRef(new ModelRef("string"))
-                .parameterType("query")
-                .required(false);
-
-        // 组装
-        params.add(i18nParameterBuilder.build());
-
-        return params;
     }
 
 
