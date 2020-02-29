0faec67d6d4b25f7d8a26bde13da66b895b8ccaf
aaric <qlhan@incarcloud.com>
2020-02-29 10:17:36 +0800

    0.1.0-SNAPSHOT discard gradle dsl

--- ds_web_back/src/test/java/com/incar/contest/ContestDsApplicationTests.java
+++ ds_web_back/src/test/java/com/incar/contest/ContestDsApplicationTests.java
@@ -1,16 +1,12 @@
 package com.incar.contest;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class ContestDsApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void testSayHello() {
+        System.out.println("hello world");
+    }
 
 }
